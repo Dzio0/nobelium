d15a597852cb7535c41d0dc81eb9ffa493d09529
Dzio <102005008+Dzio0@users.noreply.github.com>
2022-03-29 21:10:29 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,17 +1,17 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
+  title: 'DZIO',
+  author: 'Dzio',
+  email: 'yueerpap@Gmail.com',
   link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  description: '苟利国家生死以',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2021, // If leave this empty, current year will be used.
-  postsPerPage: 7,
+  since: 2016-2022, // If leave this empty, current year will be used.
+  postsPerPage: 8,
   sortByDate: false,
   showAbout: true,
   showArchive: true,
